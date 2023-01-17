20a5167b92cf37f4bc73fdd4e41967fa44a69b79
stallyonsDev <98030644+stallyonsDev@users.noreply.github.com>
2023-01-17 19:55:46 +0500

    task

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import { Stack } from '@mui/system';
+import Clone from './components/Clone';
+
+function App() {
+  return (
+    <Stack>
+      <Clone/>
+    </Stack>
+  );
+}
+
+export default App;
