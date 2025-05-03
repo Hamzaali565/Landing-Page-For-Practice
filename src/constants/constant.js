ee8d418de8315c4c37aef8a03345d09b649edc66
Hamzaali565 <ha6153806@gmail.com>
2025-05-03 22:30:51 +0500

    ch.

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,2 +1,2 @@
-// export const url = "https://eos-server.vercel.app/api/v1";
-export const url = "http://localhost:3001/api/v1";
+export const url = "https://eos-server.vercel.app/api/v1";
+// export const url = "http://localhost:3001/api/v1";
